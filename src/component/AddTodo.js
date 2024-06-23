20634942a3800d0bf042a9920b0a49cf955d92d0
Ousmane Diallo <106568955+ousmanediallo826@users.noreply.github.com>
2024-06-23 23:15:27 +0000

    add date and testing

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,47 +1,65 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
-  // Create a local react state of the this component with both content date property set to nothing.
   constructor() {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      due: null,
+      todos: []
     };
   }
-  // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
-  // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
-  // into the text field.
+
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
-      date: Date().toLocaleString('en-US')
+      date: new Date().toLocaleString('en-US')
     });
   };
-  // The handleSubmit function collects the forms input and puts it into the react state.
-  // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
-  // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
-  // in the Home.js file which then adds the input into the list.
+
+  handleDueDateChange = (date) => {
+    this.setState({
+      due: date,
+    });
+  };
+
   handleSubmit = (event) => {
     event.preventDefault();
-    if (this.state.content.trim()) {
-      this.props.addTodo(this.state);
-      this.setState({
-        content: "",
-        date: ""
-      });
+    const { content, due } = this.state;
+
+    if (content.trim() === "" || due === null || isNaN(Date.parse(due))) {
+      return;
     }
+
+    const exists = this.state.todos.find(t => t.content === content);
+    if (exists) {
+      return;
+    }
+
+    const todo = {
+      id: Math.random(),
+      content: content,
+      due: due.toLocaleDateString(),
+    };
+    this.props.addTodo(todo);
+
+    this.setState(prevState => ({
+      todos: [...prevState.todos, todo],
+      content: "",
+      date: "",
+      due: null
+    }));
+  
+    // Reset the due date after adding a todo
+    this.handleDueDateChange(null);
   };
+
   render() {
     return (
-      // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
-      // one div element.
-      // 2. The return statement should include a text field input with the handleChange function from above that
-      // is passed into an onChange event.
-      // 3. The return should also include a button with the handleSubmit function from above that is passed into
-      // an OnClick event.
-      // 4. The value of the text field also should reflect the local state of this component.
       <div>
         <TextField
           label="Add New Item"
@@ -49,6 +67,15 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.due}
+            onChange={this.handleDueDateChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
@@ -61,5 +88,4 @@ class AddTodo extends Component {
     );
   }
 }
-
 export default AddTodo;
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,29 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App'; // Assuming App is your main component
+
+test('test that App component renders Task', () => {
+    render(<App />);
+
+   
+    const inputTask = screen.getByLabelText('Add New Item');
+    const inputDate = screen.getByPlaceholderText('mm/dd/yyyy');
+    const addButton = screen.getByRole('button', { name: 'Add' });
+
+    const taskName = 'History Test';
+    const dueDate = '05/30/2023';
+
+    fireEvent.change(inputTask, { target: { value: taskName } });
+    fireEvent.change(inputDate, { target: { value: dueDate } });
+    fireEvent.click(addButton);
+
+   
+    const checkTask = screen.getByText(taskName, { exact: false });
+    const checkDueDate = screen.getByText(new RegExp(dueDate, 'i'));
+
+    expect(checkTask).toBeInTheDocument();
+    expect(checkDueDate).toBeInTheDocument();
+});
